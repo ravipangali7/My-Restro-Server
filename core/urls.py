0140a85b19898f6f1cf76771c90d8900e307a2ef
Marmajan Rokaya <marmajanrokaya6@gmail.com>
2026-03-04 14:27:21 +0545

    changes

--- core/urls.py
+++ core/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('auth/login/', auth_views.login),
     path('auth/me/', auth_views.me),
     path('auth/logout/', auth_views.logout),
+    path('auth/menu/', auth_views.menu),
 ]
